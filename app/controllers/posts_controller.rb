12d7596700f46f80ddd58523082fff1564f9709c
Salomon Romano <salo_romano@yahoo.com>
2018-05-20 16:44:04 -0500

    Create and Update - Part 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def show
+		@post = Post.find(params[:id])
+	end	
 	
 	def new
 		@post = Post.new
@@ -18,19 +22,23 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		
-	end
-
-	def update
-		
-	end
-
-	def show
 		@post = Post.find(params[:id])
 	end
 
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update_attributes(post_params)
+			redirect_to post_path, :notice => "Your post has been updated"
+		else
+			render "edit"
+		end
+	end
+
 	def destroy
-		
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path, :notice => "Your post has been deleted"
 	end
 
 	def post_params
