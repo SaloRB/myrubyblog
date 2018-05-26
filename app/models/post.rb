302fb046782f47c17383714757555cd13b362077
Salomon Romano <salo_romano@yahoo.com>
2018-05-26 12:23:29 -0500

    Meta Search, Comments and User Post Association - Part 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
 	# attr_accessible :tile, :body, :category_id, :author_id
 	belongs_to :category
-	belongs_to :admin_user 
+	belongs_to :admin_user
+	has_many :post_comments, :dependent => :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180526070009) do
+ActiveRecord::Schema.define(version: 20180526165042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(version: 20180526070009) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "post_comments", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -62,4 +72,5 @@ ActiveRecord::Schema.define(version: 20180526070009) do
     t.integer "admin_user_id"
   end
 
+  add_foreign_key "post_comments", "posts"
 end
