f8a481e0ac7bc50569df765370ec3e9e4b0fc169
Salomon Romano <salo_romano@yahoo.com>
2018-05-27 21:01:35 -0500

    Added index to app/admin/messages.rb

--- app/admin/messages.rb
+++ app/admin/messages.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Message do
 	permit_params :name, :email, :body
+	index do
+		column "ID",:id
+		column :name
+		column :email
+		column "Message", :body
+		column :created_at
+		actions
+	end
 end
