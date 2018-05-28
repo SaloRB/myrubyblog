93c0e2e215d4efa2ad2d6220a053d0e112f2369f
Salomon Romano <salo_romano@yahoo.com>
2018-05-28 10:33:30 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -75,9 +75,9 @@ ActiveRecord::Schema.define(version: 20180527195546) do
     t.string "title"
     t.text "body"
     t.integer "category_id"
+    t.integer "admin_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "admin_user_id"
   end
 
   add_foreign_key "post_comments", "posts"
